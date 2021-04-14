043e876e760528d0fb103a200911bca5cbc2aaa1
Teetirat Veangngoen <teetirat.vea@truedigital.com>
2021-04-14 18:06:54 +0700

    first commit

---
+++ src/test/java/com/demo/skaffold/SkaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.skaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
