9655d28b865d511c59a8bdc126702e3161b19e48
Teetirat Veangngoen <teetirat.vea@truedigital.com>
2021-05-03 18:49:51 +0700

    improve performance and security

--- src/main/java/com/demo/skaffold/config/MyConfig.java
+++ src/main/java/com/demo/skaffold/config/MyConfig.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @ConfigurationProperties(prefix = "bean")
 @Data
 public class MyConfig {
